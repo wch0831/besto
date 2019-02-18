ad5b5d250cba5471fb9be5ba8bcae86ade00d230
User <User@DESKTOP-M3AO6KS>
2019-02-18 20:32:40 +0900

    create game 진행중

--- besto_prj/src/main/java/com/com/game/GameServiceImpl.java
+++ besto_prj/src/main/java/com/com/game/GameServiceImpl.java
@@ -1,5 +1,17 @@
 package com.com.game;
 
-public class GameServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.com.mapper.GameMapper;
+
+@Service
+public class GameServiceImpl {
+	
+	@Autowired
+	private GameMapper gameMapper;
+	
+	public int svcGameInsert() {
+		return gameMapper.GameInsert();
+	}
 }
