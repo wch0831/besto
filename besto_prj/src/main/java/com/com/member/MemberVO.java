85a4d840d02e393a924bf06064c3d6f72a62f62d
kosmo-18 <kosmo-18@DESKTOP-OE8BMI9>
2019-02-18 18:17:42 +0900

    wch

---
+++ besto_prj/src/main/java/com/com/member/MemberVO.java
@@ -0,0 +1,96 @@
+package com.com.member;
+
+public class MemberVO {
+	private int users_seq;
+	private String users_id;
+	private String users_pw;
+	private String users_name;
+	private String users_email;
+	private String users_address;
+	private String users_detail_address;
+	private String users_phone;
+	private String users_birth;
+	private String users_bank;
+	private String users_account;
+	private String users_del;
+	private String users_grant;
+	
+	public int getUsers_seq() {
+		return users_seq;
+	}
+	public void setUsers_seq(int users_seq) {
+		this.users_seq = users_seq;
+	}
+	public String getUsers_id() {
+		return users_id;
+	}
+	public void setUsers_id(String users_id) {
+		this.users_id = users_id;
+	}
+	public String getUsers_pw() {
+		return users_pw;
+	}
+	public void setUsers_pw(String users_pw) {
+		this.users_pw = users_pw;
+	}
+	public String getUsers_name() {
+		return users_name;
+	}
+	public void setUsers_name(String users_name) {
+		this.users_name = users_name;
+	}
+	public String getUsers_email() {
+		return users_email;
+	}
+	public void setUsers_email(String users_email) {
+		this.users_email = users_email;
+	}
+	public String getUsers_address() {
+		return users_address;
+	}
+	public void setUsers_address(String users_address) {
+		this.users_address = users_address;
+	}
+	public String getUsers_detail_address() {
+		return users_detail_address;
+	}
+	public void setUsers_detail_address(String users_detail_address) {
+		this.users_detail_address = users_detail_address;
+	}
+	public String getUsers_phone() {
+		return users_phone;
+	}
+	public void setUsers_phone(String users_phone) {
+		this.users_phone = users_phone;
+	}
+	public String getUsers_birth() {
+		return users_birth;
+	}
+	public void setUsers_birth(String users_birth) {
+		this.users_birth = users_birth;
+	}
+	public String getUsers_bank() {
+		return users_bank;
+	}
+	public void setUsers_bank(String users_bank) {
+		this.users_bank = users_bank;
+	}
+	public String getUsers_account() {
+		return users_account;
+	}
+	public void setUsers_account(String users_account) {
+		this.users_account = users_account;
+	}
+	public String getUsers_del() {
+		return users_del;
+	}
+	public void setUsers_del(String users_del) {
+		this.users_del = users_del;
+	}
+	public String getUsers_grant() {
+		return users_grant;
+	}
+	public void setUsers_grant(String users_grant) {
+		this.users_grant = users_grant;
+	}
+}
