54594b34dab4bf5a7bda7b31385c8ebab4c08655
kosmo-18 <kosmo-18@DESKTOP-OE8BMI9>
2019-02-19 18:08:52 +0900

    18:08wch

--- besto_prj/src/main/java/com/com/member/UserService.java
+++ besto_prj/src/main/java/com/com/member/UserService.java
@@ -5,10 +5,10 @@ import com.com.member.MemberVO;
 public interface UserService {
 
 	public MemberVO login(MemberVO vo) throws Exception;
-	public void memberRegister(MemberVO vo) throws Exception;
+	public int memberRegister(MemberVO vo) throws Exception;
 	public MemberVO memberDetail(String uid) throws Exception;
-	public void memberDelete(MemberVO vo) throws Exception;
-	public void memberUpdate(MemberVO vo) throws Exception;
-	public void changePassword(MemberVO vo) throws Exception;
+	public int memberDelete(MemberVO vo) throws Exception;
+	public int memberUpdate(MemberVO vo) throws Exception;
+	public int changePassword(MemberVO vo) throws Exception;
 	public String findId(String uemail) throws Exception;
 }
