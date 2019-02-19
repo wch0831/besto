6714ab041252a8a22bd73c1dfec59323127efaba
kosmo-18 <kosmo-18@DESKTOP-OE8BMI9>
2019-02-19 15:02:54 +0900

    update

--- besto_prj/src/main/java/com/com/game/GameServiceImpl.java
+++ besto_prj/src/main/java/com/com/game/GameServiceImpl.java
@@ -8,27 +8,12 @@ import org.springframework.stereotype.Service;
 import com.com.mapper.GameMapper;
 
 @Service
-<<<<<<< HEAD
-public class GameServiceImpl implements GameService {
-=======
 public class GameServiceImpl implements GameService{
->>>>>>> branch 'master' of https://github.com/wch0831/besto
 	
 	@Autowired
 	private GameMapper gameMapper;
-<<<<<<< HEAD
 	
 //	public int svcGameInsert() {
 //		return gameMapper.GameInsert();
 //	}
-=======
-
-	@Override
-	public ArrayList<GameVO> svcGameInsert() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
->>>>>>> branch 'master' of https://github.com/wch0831/besto
 }
