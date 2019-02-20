59d96b55c112a861ab52d52377dd1a5b0af51e8d
kosmo-07 <kosmo-07@DESKTOP-QRR0QTO>
2019-02-21 01:52:51 +0900

    ㅓㅓ

--- besto_prj/src/main/java/com/com/game/GameRestController.java
+++ besto_prj/src/main/java/com/com/game/GameRestController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class GameRestController {
