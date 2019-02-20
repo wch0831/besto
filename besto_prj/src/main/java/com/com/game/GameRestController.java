9b9e702bb5dc681e20b80a48a38b381de617a2b9
User <User@DESKTOP-M3AO6KS>
2019-02-20 15:39:46 +0900

    관리자 insert 완료 문경아 아리가또

--- besto_prj/src/main/java/com/com/game/GameRestController.java
+++ besto_prj/src/main/java/com/com/game/GameRestController.java
@@ -5,18 +5,23 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 @RestController
 public class GameRestController {
 	@Autowired
 	private GameService gameService;
 	
-	@RequestMapping(value="/game_select_rest.do", method = RequestMethod.POST)
-	public ArrayList<MatchVO> gameInsertSelect(GameVO vo) {
-		ArrayList<MatchVO> list = gameService.svcGameSelect(vo);
-		return list;
-	}
+//	@RequestMapping(value="/game_select_rest.do", method = RequestMethod.POST)
+//	public ArrayList<MatchVO> gameInsertSelect(GameVO vo) {
+//		ArrayList<MatchVO> list = gameService.svcGameSelect(vo);
+//		return list;
+//	}
+	
 
 }
--- besto_prj/src/main/java/com/com/game/GameService.java
+++ besto_prj/src/main/java/com/com/game/GameService.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public interface GameService {
 	
 	public int svcGameInsert(GameVO vo);
-	public int svcMatchInsert(GameVO vo);
+	public int svcMatchInsert(ArrayList<MatchVO> list);
+	public int svcSelectSeq();
 	
 	public ArrayList<MatchVO> svcGameSelect(GameVO vo);
 	
