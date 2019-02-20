fc2fd60edc20ed70d9004cafc4953047eaa40791
kosmo-17 <kosmo-17@DESKTOP-NHMBE56>
2019-02-20 14:52:25 +0900

    1452 hj

--- besto_prj/src/main/java/com/com/mapper/BoardMapper.java
+++ besto_prj/src/main/java/com/com/mapper/BoardMapper.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.com.board.BoardFreeVO;
+import com.com.board.NoticeVO;
 import com.com.board.BoardChallengeVO;
 
 @Mapper
@@ -13,6 +14,6 @@ public interface BoardMapper{
 	//public BoardFreeVO free_board_insert();
 	public ArrayList<BoardFreeVO> free_board_select();
 	public ArrayList<BoardChallengeVO> challenge_board_select();
-	
+	public ArrayList<NoticeVO> notice_all();
 	
 }
